2a62b1a39367ddabd5cf5f468c10285be6888c1f
Mikhail Gorbadey <gormisha05@yandex.ru>
2022-08-31 15:51:26 +0300

    cell changes color by clicking on it

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Board } from "../models/Board";
+import { Cell } from "../models/Cell";
 import CellComponent from "./CellComponent";
 
 interface BoardProps {
@@ -8,14 +9,22 @@ interface BoardProps {
 }
 
 const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
+  const [selectedCell, setSelectedCell] = useState <Cell | null>(null)
+
+  function click(cell: Cell) {
+    setSelectedCell(cell)
+  }
+
   return (
     <div className='board'>
       {board.cells.map((row, index) =>
         <React.Fragment key={index}>
           {row.map(cell =>
             <CellComponent
-            cell={cell}
-            key={cell.id}
+              click={click}
+              cell={cell}
+              key={cell.id}
+              selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
             />)}
         </React.Fragment>
       )}
