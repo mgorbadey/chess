c475e8b72a1789137e98700a60e3edecc418784b
Mikhail Gorbadey <gormisha05@yandex.ru>
2022-09-01 17:22:12 +0300

    all figures movement logic exept king done

--- src/models/figures/Rook.ts
+++ src/models/figures/Rook.ts
@@ -5,7 +5,7 @@ import blackLogo from '../../assets/black-rook.png'
 import whiteLogo from '../../assets/white-rook.png'
 
 export class Rook extends Figure {
-  constructor(color: Colors, cell: Cell){
+  constructor(color: Colors, cell: Cell) {
     super(color, cell);
     this.logo = color === Colors.BLACK ? blackLogo : whiteLogo
     this.name = FigureNames.ROOK
@@ -14,6 +14,10 @@ export class Rook extends Figure {
   canMove(target: Cell): boolean {
     if (!super.canMove(target))
       return false;
-    return true
+    if (this.cell.isEmptyVertical(target))
+      return true;
+    if (this.cell.isEmptyHorizontal(target))
+      return true;
+    return false
   }
 }
