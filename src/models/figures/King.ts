7cd116ae8a7ff6c8af2527e70514d3b4628c6891
Mikhail Gorbadey <gormisha05@yandex.ru>
2022-09-01 17:53:43 +0300

    swapping players

--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -5,7 +5,7 @@ import blackLogo from '../../assets/black-king.png'
 import whiteLogo from '../../assets/white-king.png'
 
 export class King extends Figure {
-  constructor(color: Colors, cell: Cell){
+  constructor(color: Colors, cell: Cell) {
     super(color, cell);
     this.logo = color === Colors.BLACK ? blackLogo : whiteLogo
     this.name = FigureNames.KING
@@ -14,9 +14,9 @@ export class King extends Figure {
   canMove(target: Cell): boolean {
     if (!super.canMove(target))
       return false;
-      const dx = Math.abs(this.cell.x - target.x);
-      const dy = Math.abs(this.cell.y - target.y);
-  
-      return (dx === 1 && dy === 1) || (dx === 1 && dy === 1) || (dx === 1 && dy === 0) || (dx === 0 && dy === 1)
+    const dx = Math.abs(this.cell.x - target.x);
+    const dy = Math.abs(this.cell.y - target.y);
+
+    return (dx === 1 && dy === 1) || (dx === 1 && dy === 1) || (dx === 1 && dy === 0) || (dx === 0 && dy === 1)
   }
 }
